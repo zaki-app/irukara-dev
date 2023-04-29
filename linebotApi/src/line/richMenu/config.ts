235c721c3ccba35192c44819fcea2dbff9212b5b
yozakura <matchnokaori@gmail.com>
2023-04-29 11:17:29 +0900

    リッチメニュー修正、huskey導入

--- linebotApi/src/line/richMenu/config.ts
+++ linebotApi/src/line/richMenu/config.ts
@@ -3,7 +3,7 @@ import type { RichMenu } from '@line/bot-sdk';
 export const richMenuConfig: RichMenu = {
   size: {
     width: 2500,
-    height: 400,
+    height: 843,
   },
   selected: false,
   name: 'irukara-rich-menu',
@@ -13,39 +13,39 @@ export const richMenuConfig: RichMenu = {
       bounds: {
         x: 0,
         y: 0,
-        width: 800,
-        height: 400,
+        width: 833,
+        height: 843,
       },
       action: {
         type: 'uri',
         label: 'irukara',
-        uri: 'https://staging.d2hjuf0oqfobak.amplifyapp.com/',
+        uri: process.env.LIFF_URL,
       },
     },
     {
       bounds: {
-        x: 0,
-        y: 843,
-        width: 800,
-        height: 400,
+        x: 833,
+        y: 0,
+        width: 833,
+        height: 843,
       },
       action: {
         type: 'uri',
-        label: 'irukara',
-        uri: 'https://staging.d2hjuf0oqfobak.amplifyapp.com/',
+        label: '使い方',
+        uri: `${process.env.LIFF_URL}/usage`,
       },
     },
     {
       bounds: {
-        x: 0,
-        y: 843,
-        width: 843,
-        height: 400,
+        x: 1667,
+        y: 0,
+        width: 833,
+        height: 843,
       },
       action: {
         type: 'uri',
-        label: 'irukara',
-        uri: 'https://staging.d2hjuf0oqfobak.amplifyapp.com/',
+        label: 'お問い合わせ',
+        uri: `${process.env.LIFF_URL}/contact`,
       },
     },
   ],
